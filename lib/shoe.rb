8e987e7dcc6ace7fd5155cc48cb8c174184da8e0
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-08 18:31:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,17 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-
+  attr_accessor :brand
+ 
+  BRANDS = []
+ 
   def initialize(brand)
     @brand = brand
   end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+ 
+  # create the writer for genre and add the logic for the class constant
+  def brand=(brand)
+    @brand = brand
+    if !(BRANDS.include?(brand))
+      BRANDS << brand
+    end
   end
-
 end
